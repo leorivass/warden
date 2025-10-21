0ae57b3ef4eb62a63c77ee3703fe4769c654e12d
Carlos Rivas <leorivas1805@gmail.com>
2025-10-20 21:12:08 -0400

    mcu: update main.c to test the I2C driver
    - Commented out all USART-related code since it was used only for debugging and is not required in the final project.

--- mcu/main.c
+++ mcu/main.c
@@ -1,21 +1,26 @@
-#include "gpio.h"
-#include "usart.h"
 #include <avr/io.h>
-#include <avr/interrupt.h>
 #include <util/delay.h>
+// #include <avr/interrupt.h> 
+// #include "usart.h" // Just for debugging
+#include "i2c.h"
+
+#define LCD_ADDRESS 0x27
+#define TURN_OFF_BACKLIGHT 0x4
+#define TURN_ON_BACKLIGHT  0xC
 
 int main() {
 
-    cli();
-    serial_begin(9600);
-    sei();
+    int ret;
 
-    while (1) {
-        serial_println("Hi, I'm testing this driver with screen terminal");
-        _delay_ms(1000);
-        serial_println("It seems to work properly!");
-        _delay_ms(1000);
-    }
+    // cli();
+    // serial_begin(9600);
+    i2c_init();
+    // sei();
+
+    i2c_write(LCD_ADDRESS, TURN_OFF_BACKLIGHT);
+    i2c_write(LCD_ADDRESS, 0x0);
+    
+    while(1);
 
     return 0;
 }
