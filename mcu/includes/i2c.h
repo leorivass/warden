d1220a4556acdbfba76e5d1ab595851048dc8a0f
Carlos Rivas <leorivas1805@gmail.com>
2025-10-23 14:56:03 -0400

    mcu: add a function to read multiple registers with one I2C transaction

--- mcu/includes/i2c.h
+++ mcu/includes/i2c.h
@@ -6,5 +6,6 @@
 void i2c_init();
 int i2c_write_register(uint8_t, uint8_t, uint8_t);
 int i2c_read_register(uint8_t, uint8_t, uint8_t*);
+int i2c_burst_read_registers(uint8_t, uint8_t, uint8_t*, uint8_t);
 
 #endif
--- mcu/src/i2c.c
+++ mcu/src/i2c.c
@@ -6,6 +6,7 @@
 #define MT_SLA_ACK              0x18
 #define MT_DATA_ACK             0x28
 #define MR_SLA_ACK              0x40
+#define MR_DATA_ACK             0x50
 #define MR_DATA_NACK            0x58
 
 #define send_start()\
@@ -14,6 +15,8 @@
     TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN)
 #define set_twint()\
     TWCR = (1 << TWINT) | (1 << TWEN)
+#define send_ack()\
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA)
 
 void i2c_init() {
     /* Set I2C transmission speed to 400Khz (fast mode) */
@@ -154,6 +157,100 @@ int i2c_read_register(uint8_t slave_address, uint8_t reg_address, uint8_t *data)
     }
 
     *data = TWDR;
+
+    send_stop();
+
+    return 0;
+}
+
+int i2c_burst_read_registers(uint8_t slave_address, uint8_t start_reg, uint8_t *buffer, uint8_t length) {
+
+    send_start();
+
+    /* Wait for TWINT flag is set by hardware to start transmission */
+    while(!(TWCR & (1 << TWINT)));
+
+    /* If the status code is not 0x08 after sending send_start(), then there was an error*/
+    if ((TWSR & 0xF8) != START_STATUS_CODE) {
+        send_stop();
+        return -1;
+    }
+
+    /* Load slave address + WRITE bit into TWDR register */ 
+    TWDR = slave_address << 1;
+
+    /* Set TWINT to clear the TWINT flag and start transmission */
+    set_twint();
+
+    while(!(TWCR & (1 << TWINT)));
+
+    /* If the status code is 0x20 after sending SLAVE_W, NOT_ACK has been received */
+    /* If status code == 0x18, ACK has been received */
+    if ((TWSR & 0xF8) != MT_SLA_ACK) {
+        send_stop(); 
+        return -2;
+    }
+
+    TWDR = start_reg;
+
+    set_twint();
+
+    while(!(TWCR & (1 << TWINT)));
+
+    /* If the status code is 0x30 after sending DATA, NOT_ACK has been received */
+    /* If status code == 0x28, ACK has been received */
+    if ((TWSR & 0xF8) != MT_DATA_ACK) {
+        send_stop();
+        return -3;
+    }
+
+    /* Send a repeated start condition to read the register */
+    send_start();
+
+    while(!(TWCR & (1 << TWINT)));
+
+    if ((TWSR & 0xF8) != REPEATED_START_CODE) {
+        send_stop();
+        return -4;
+    }
+
+    /* Load slave address + READ bit into TWDR register */
+    TWDR = (slave_address << 1) | 0x01;
+
+    set_twint();
+
+    while(!(TWCR & (1 << TWINT)));
+
+    if ((TWSR & 0xF8) != MR_SLA_ACK) {
+        send_stop();
+        return -5;
+    }
+
+    for(uint8_t i = 0; i < length; i++) {
+
+        uint8_t STATUS_CODE;
+
+        if (i == (length - 1)) {
+            /* Read one byte and send NACK, for this is the last reading */
+            set_twint();
+            STATUS_CODE = MR_DATA_NACK;
+        }
+        else {
+            /* Read one byte and send ACK, for we want to keep reading */
+            send_ack();
+            STATUS_CODE = MR_DATA_ACK;
+        }
+
+        while(!(TWCR & (1 << TWINT)));
+
+        if ((TWSR & 0xF8) != STATUS_CODE) {
+            send_stop();
+            return -6;
+        }
+
+        buffer[i] = TWDR;
+
+    }
 
     send_stop();
 
